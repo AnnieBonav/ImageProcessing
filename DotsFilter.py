a6ad5537852d37d44c478d3e8115a4dcb22aabd5
Annie Bonav <anniebonav@outlook.com>
2024-05-03 21:43:33 -0600

    Simplifies Dots filter

--- DotsFilter.py
+++ DotsFilter.py
@@ -2,38 +2,30 @@ import sys, time, cv2
 import numpy as np
 from skimage import draw
 
-# BRUSHES does not seem to do anything
-BRUSHES = 100
 flowers1 = "TestImages/FlowersVase1.jpeg"
 
-def process(inputImage, brushSize):
+def filterImage(inputImage, brushSize):
     start = time.time()
     
     brushSizeInt = int(brushSize)
     margin = brushSizeInt * 2
     halfBrushSizeInt = brushSizeInt // 2
     
-    shape = ((inputImage.shape[0] - 2 * margin) // brushSizeInt, (inputImage.shape[1] - 2 * margin) // brushSizeInt)
-    brushes = [draw.disk((halfBrushSizeInt, halfBrushSizeInt), halfBrushSizeInt) for _ in range(BRUSHES)]
-
+    # shape = ((inputImage.shape[0] - 2 * margin) // brushSizeInt, (inputImage.shape[1] - 2 * margin) // brushSizeInt)
+    brush = draw.disk((halfBrushSizeInt, halfBrushSizeInt), halfBrushSizeInt)
     result = np.zeros(inputImage.shape, dtype=np.uint8)
 
     for x in range(margin, inputImage.shape[0] - margin, brushSizeInt):
         for y in range(margin, inputImage.shape[1] - margin, brushSizeInt):
-            diskXs, diskYs = brushes[0]
-            result[x + diskXs, y + diskYs] = inputImage[x, y]
+            brushX, brushY = brush
+            result[x + brushX, y + brushY] = inputImage[x, y]
     
     return result, time.time() - start
 
 inputImage = cv2.imread(flowers1)
-brushSize = 10.0
+brushSize = 20.0
 
-if inputImage.ndim < 3:
-    sys.exit('Only RGB or RGBA images supported.')
-elif inputImage.ndim == 4:
-    inputImage = inputImage[:, :, :3]
-
-result, duration = process(inputImage, brushSize = brushSize)
+result, duration = filterImage(inputImage, brushSize = brushSize)
 print('Processed in', round(duration, 2), 'seconds.')
 
 def showImage(img):
@@ -43,6 +35,6 @@ def showImage(img):
 
 showImage(result)
 folderPath = "TestingBrush"
-outputImage = f"BrushSize{brushSize}Brushes{BRUSHES}.jpg"
+outputImage = f"BrushSize{brushSize}.jpg"
 
 cv2.imwrite(f"{folderPath}/{outputImage}", result)
